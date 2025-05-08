73942eedde3f71d0d7d90c0f5456478c4b8ce902
Pedro Ferreira <ferfonhp@gmail.com>
2025-05-08 08:55:20 -0300

    construtor do produto

--- src/appCaixaMercado/Produto.java
+++ src/appCaixaMercado/Produto.java
@@ -5,6 +5,12 @@ public class Produto {
     private String nome;
     private float preco;
 
+    public Produto(int codigo, String nome, float preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
     public int getCodigo() {
         return codigo;
     }
