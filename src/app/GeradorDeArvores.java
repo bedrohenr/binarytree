be19080f525e4d7789deb9fb514d2fd5f05b5ab2
bedrohenr <ferfonhp@gmail.com>
2025-05-11 19:48:07 -0300

    resolvendo erros de import

--- src/app/GeradorDeArvores.java
+++ src/app/GeradorDeArvores.java
@@ -6,8 +6,7 @@
 package app;
 
 import java.util.Random;
-//Ajuste este import para que sua classe de Arvore Binária seja importada
-import lib.ArvoreBinariaExemplo;
+
 //Aqui estou importando a interface IArvoreBinaria, a mesma que sua classe de Árvore Binaria deve implementar
 import lib.IArvoreBinaria;
 
